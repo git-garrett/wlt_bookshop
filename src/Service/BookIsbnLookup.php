95e6c531b83009f956ab8d783d925e3d8e52f177
GPerks <4460511+git-garrett@users.noreply.github.com>
2025-12-11 16:20:36 -0800

    increase rate limit

--- src/Service/BookIsbnLookup.php
+++ src/Service/BookIsbnLookup.php
@@ -17,7 +17,7 @@ class BookIsbnLookup {
   public const API_STAT_KEYS = ['search', 'edition', 'work'];
 
   private const OPEN_LIBRARY_USER_AGENT = 'WorldLiteratureToday (staff@evenvision.com)';
-  private const OPEN_LIBRARY_RATE_LIMIT = 1; 
+  private const OPEN_LIBRARY_RATE_LIMIT = 10; 
   private const OPEN_LIBRARY_WINDOW_SECONDS = 3; 
 
   /** @var \GuzzleHttp\ClientInterface */
